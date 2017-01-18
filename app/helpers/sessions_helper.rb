018871143c157a5f6acf3aa13b09ea10a0196a9b
Taylor Boykin <taylorboykin@arrayschool.com>
2017-01-18 16:07:08 -0700

    finish basic login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,5 +1,22 @@
 module SessionsHelper
+
+  # logs in the given user
   def log_in(user)
     session[:user_id] = user.id
   end
-end
+
+  # logs out the current user
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
+
+  # returns the current logged user
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
+end # SessionsHelper Module
