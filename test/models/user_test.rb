4f7756c4fa0c187ac0bf858638edeb7ad59ed7fe
Taylor Boykin <taylorboykin@arrayschool.com>
2017-01-19 16:46:05 -0700

    add a remember me checkbox

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -85,4 +85,8 @@ class UserTest < ActiveSupport::TestCase
     @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
   end #password should have a minimum length
+
+  test "authenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticated?('')
+  end #authenticated? should return false for a user with nil digest
 end
